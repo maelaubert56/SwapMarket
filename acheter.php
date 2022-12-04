53443e6f106ab0fc706cf0e02679b2a0c4a1d294
quentin damois <quentin.damois@efrei.net>
2022-12-04 16:00:29 +0100

    modified:   acheter.php 	new file:   css/style_produit.css

--- acheter.php
+++ acheter.php
@@ -17,7 +17,7 @@
     <head>
         <title>SwapMarket</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style_produit.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
@@ -26,17 +26,32 @@
             <p>
                 <h3>Les produits</h3>
                 <?php 
-                var_dump($contenu);
+                //var_dump($contenu);
                 $i = 0;
                 while ( $i + 3 < $taille_contenue) {
                     for( $j = 0; $j < 3; $j = $j + 1) {
-                        printf("%s pour %d euros disponible chez %s.\t",$contenu[$i + $j][model],$contenu[$i + $j][price],$contenu[$i + $j][contact]);
+                        //printf("%s pour %d euros _\n_ disponible chez %s.\n",$contenu[$i + $j]["model"],$contenu[$i + $j]["price"],$contenu[$i + $j]["contact"]);
+                        ?>
+                        <div class="produit">
+                        <?php
+                        echo $contenu[$i + $j]["askbid"]," ",$contenu[$i + $j]["model"]," pour ",$contenu[$i + $j]["price"],". Pour rentrer en contact appeler le ",$contenu[$i + $j]["contact"];
+                        ?>
+                        </div>
+                        <?php
                     }
                     $i = $i + 3;
-                    printf("\n");
+                    //echo "*\n*";
                 }
                 while ($i < $taille_contenue) {
-                    printf("%s pour %d euros disponible chez %s.\t",$contenu[$i][model],$contenu[$i][price],$contenu[$i][contact]);
+                    //printf("%s pour %d euros disponible chez %s.\n",$contenu[$i]["model"],$contenu[$i]["price"],$contenu[$i]["contact"]);
+                    ?>
+                    <div class="produit">
+                    <?php
+                    echo $contenu[$i]["askbid"]," ",$contenu[$i]["model"]," pour ",$contenu[$i]["price"],". Pour rentrer en contact appeler le ",$contenu[$i]["contact"];
+                    ?>
+                    </div>
+                    <?php
+                    $i = $i + 1;
                 }
                 ?>
             </p>
